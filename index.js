7483a9ea1adf167bd56b4f53eebb2d8dbb765c11
里見健太 <satomikennta1213@gmail.com>
2019-12-13 00:07:18 +0900

    complete

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello CI/CD !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+  res.send('Hello CircleCI ');
 })
 
 app.listen(3000);
