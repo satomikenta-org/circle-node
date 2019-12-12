e1884625e29c2c72a5c7bf9ff74df34b17dc8d55
里見健太 <satomikennta1213@gmail.com>
2019-12-13 00:51:55 +0900

    modify sh

--- main.sh
+++ main.sh
@@ -5,7 +5,7 @@ ssh $HOST \
 cd app/circle-node/ && \
 git pull origin master && \
 npm i && \
-pm2 startup -f && \
+sudo pm2 startup -f && \
 pm2 delete -s myapp || :
 pm2 start ./index.js --name myapp && \
 echo Deploy END"
