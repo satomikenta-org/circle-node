6b9b375686b78062b9a9ba8410ac28e00839c6fb
里見健太 <satomikennta1213@gmail.com>
2019-12-13 00:43:20 +0900

    modify sh

--- main.sh
+++ main.sh
@@ -1,19 +1,19 @@
 #! /bin/sh
 
-ssh $HOST
+ssh $HOST \
 
-echo Start deploy
+"echo Start deploy && \
 
-cd app/circle-node/
+cd app/circle-node/ && \
 
-git pull origin master
+git pull origin master && \
 
-npm i
+npm i && \
 
-pm2 startup
+pm2 startup && \
 
 pm2 delete -s myapp || :
 
-pm2 start ./index.js --name myapp
+pm2 start ./index.js --name myapp && \
 
-echo Deploy END
+echo Deploy END"
