816744386eeccc00c38b20c20bbefd66f02d81f3
里見健太 <satomikennta1213@gmail.com>
2019-12-13 00:15:31 +0900

    add env

--- bin/deploy.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-ssh ec2-user@ec2-3-112-202-137.ap-northeast-1.compute.amazonaws.com \
-"echo Start deploy && \
-cd app/circle-node/ && \
-git pull origin master && \
-npm i && \
-pm2 delete -s myapp || :
-pm2 start ./index.js --name myapp && \
-echo Deploy end"
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello CircleCI ');
+  res.send('Hello CircleCI v2 ');
 })
 
 app.listen(3000);
